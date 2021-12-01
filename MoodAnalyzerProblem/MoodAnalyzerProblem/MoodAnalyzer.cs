e3a6074721df6323a30f23012846075b7a06f077
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-01 22:32:46 +0530

    [gunasekhar9] add : MoodAnalyzerProblem

---
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        private string Message;
+        public string AnalyzeMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/MoodAnalyzerTesting/MoodAnalyzerTest.cs
@@ -0,0 +1,35 @@
+using MoodAnalyzerProblem;
+using NUnit.Framework;
+
+namespace MoodAnalyzerTesting
+{
+    public class Tests
+    {
+        MoodAnalyzer mood;
+        [SetUp]
+        public void Setup()
+        {
+            this.mood = new MoodAnalyzer();
+        }
+        [Test]
+        public void GivenSadMessage_WhenAnalyze_ShouldReturnSad()
+        {
+            //Arrange
+            string output = "SAD";
+            //Act
+            string result = mood.AnalyzeMood("I am in Sad Mood");
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+        [Test]
+        public void GivenAnyMessage_WhenAnalyze_ShouldReturnHappy()
+        {
+            //Arrange
+            string output = "HAPPY";
+            //Act
+            string result = mood.AnalyzeMood("I am in Any Mood");
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+    }
+}
